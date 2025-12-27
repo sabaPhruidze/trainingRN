7e09b4590a02b261b3b43869abc54e648f1ee490
Saba Phruidze <sabaphruidze01@gmail.com>
2025-12-27 20:50:37 +0400

    learned how to use 2 or more styles together , it's a bit different through from react.js

--- components/Second.tsx
+++ components/Second.tsx
@@ -1,12 +1,36 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Switch, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Second = () => {
+  const [toggle, setToggle] = useState<boolean>(false);
   return (
-    <View>
-      <Text>Second</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View
+        style={[styles.box, toggle ? styles.toggleLight : styles.toggleDark]}
+      >
+        <Text>saba</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default Second;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  box: {
+    width: 100,
+    height: 100,
+  },
+  toggleLight: {
+    backgroundColor: "#002afeff",
+  },
+  toggleDark: {
+    backgroundColor: "#090471ff",
+  },
+});
