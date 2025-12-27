c9be72ca66037be78309a25f424241c9814a5153
Saba Phruidze <sabaphruidze01@gmail.com>
2025-12-27 19:56:26 +0400

    building first task

--- components/First.tsx
+++ components/First.tsx
@@ -1,12 +1,32 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 import React from "react";
 
 const First = () => {
   return (
-    <View>
-      <Text>First</Text>
+    <View style={styles.container}>
+      <Image
+        source={{
+          uri: "https://img.freepik.com/free-vector/blue-circle-with-white-user_78370-4707.jpg?semt=ais_hybrid&w=740&q=80",
+        }}
+        alt="It is an user icon with white and blue colors.In the center is a person's icon in white colors, outside it has blue and than white circle"
+        style={styles.icon}
+      />
     </View>
   );
 };
 
 export default First;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#95e600ff",
+  },
+  icon: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+  },
+});
