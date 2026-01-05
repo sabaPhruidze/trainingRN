b9589ebd3fef70dde7353d87de4cf75e99d2f0d2
Saba Phruidze <sabaphruidze01@gmail.com>
2026-01-05 11:07:26 +0400

    the logic of receiving data information is done

--- components/Ten.tsx
+++ components/Ten.tsx
@@ -1,11 +1,29 @@
 import { View, Text, StyleSheet, Pressable } from "react-native";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { RootStackParamList } from "../Main";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 
 type Props = NativeStackScreenProps<RootStackParamList, "Ten">;
 
 const Ten = ({ navigation }: Props) => {
+  const [todos, setTodos] = useState();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  const receiveData = async () => {
+    try {
+      const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+      const json = await res.json();
+      setTodos(json);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    receiveData();
+  }, []);
   return (
     <View>
       <Pressable
